ab791d7a4fb6db8ed0b101d3ba3e1def0a597974
Elquer Carlos <msn@elquer.com>
2026-04-27 14:27:23 -0300

    feat: identidade One-Man Show, Sobre, Projetos, Contato, Newsletter, Devlog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Elquer Carlos";
+export const SITE_DESCRIPTION = "Dev, Ator, Pai. Documentando a construção da Larissa em público.";
